f5509b0b092654251edfa8f42c39eb29e99a99dd
Jarold Wong <jaroldw@gmail.com>
2016-03-04 13:43:18 -0800

    Install seed dump plugin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-Department.create(name: 'Economics')
-Department.create(name: 'History')
-Department.create(name: 'Philosophy')
-Department.create(name: 'Political Science')
-Department.create(name: 'Sociology')
-
-Location.create(building: 'SSH', room: '130')
-Location.create(building: 'SSH', room: '131')
+Department.create!([
+  {name: "Economics"},
+  {name: "History"},
+  {name: "Philosophy"},
+  {name: "Political Science"},
+  {name: "Sociology"}
+])
+DepartmentLocation.create!([
+  {department_id: 2, location_id: 3},
+  {department_id: 2, location_id: 5},
+  {department_id: 2, location_id: 4}
+])
+DepartmentPerson.create!([
+  {department_id: 2, person_id: 1},
+  {department_id: 2, person_id: 2}
+])
+Key.create!([
+  {serial_no: "E1TF", key_class: "1", person_id: 1, issued: true},
+  {serial_no: "E1TD", key_class: "1", person_id: 2, issued: true},
+  {serial_no: "E1TK83", key_class: "5", person_id: 6, issued: true},
+  {serial_no: "E1TK83", key_class: "4", person_id: 5, issued: true},
+  {serial_no: "E1TH70", key_class: "4", person_id: 6, issued: true},
+  {serial_no: "E1TH70", key_class: "1", person_id: 5, issued: true},
+  {serial_no: "A1YA50", key_class: "2", person_id: 5, issued: true},
+  {serial_no: "E1TK82", key_class: "10", person_id: 4, issued: true}
+])
+Location.create!([
+  {building: "SSH", room: "130"},
+  {building: "SSH", room: "131"},
+  {building: "SSH", room: "2216"},
+  {building: "SSH", room: "2211"},
+  {building: "SSH", room: "2208"}
+])
+Person.create!([
+  {name: "Anya Gibson", loginid: "abschwin"},
+  {name: "Monica Fischer", loginid: "mlfische"},
+  {name: "Kathy Miner", loginid: "keminer"},
+  {name: "Janine Carlson", loginid: "jmcarlso"},
+  {name: "Ben Leung", loginid: "nefful"},
+  {name: "Brandon Baker", loginid: "bvbaker"}
+])
+PersonRole.create!([
+  {person_id: 3, role_id: 2}
+])
+Role.create!([
+  {name: "administrator"},
+  {name: "keymaster"}
+])
